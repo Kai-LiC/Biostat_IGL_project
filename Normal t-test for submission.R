336724f4fb46e25396346fc68134361bdd4a9010
Kai-LiC <kaili.chang@kaust.edu.sa>
2026-05-08 23:42:13 +0300

    Update Normal t-test for submission.R

--- Normal t-test for submission.R
+++ Normal t-test for submission.R
@@ -1,3 +1,10 @@
+###README###
+### 1.Test Normality for the data: Shapiro-Wilk normality test(p-value>0.05)
+##-------------If normally distributed:Stick with wald t test
+##-------------If not, use Mann-Whitney U test
+### 2.Multiple-comparisons correction
+### 3. Plotting
+
 ###Normal T test###
 
 library(readxl)
